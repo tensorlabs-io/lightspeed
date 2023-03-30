cc34236570b7277e5216b04057123abe394ed67a
hassaan-zamir <hassaanzamir94@gmail.com>
2023-03-31 00:07:05 +0500

    vercel deploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps, AppContext } from 'next/app'
+import App from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
 }
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await App.getInitialProps(appContext);
+  return { ...appProps, pageProps: {} };
+};
+
+export default MyApp;
